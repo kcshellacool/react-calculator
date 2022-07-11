de93b874681529986e1f8e7de1b7be357f55543b
kcshellacool <hudi1007@gmail.com>
2022-07-10 22:02:34 -0700

    updated Operationbuttonjs

--- src/DigitButton.js
+++ src/DigitButton.js
@@ -1,6 +1,11 @@
-import { ACTIONS } from './App.js'
-export default function DigitButton({ dispatch, digit}) {
- return <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: {digit} })}>
-    {digit}
+import { ACTIONS } from "./App"
+
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
     </button>
+  )
 }
